16254a2e74d55e503c048d2c22a46367b494c19d
MichaelLugo313 <michaellugo@gmail.com>
2025-07-09 18:21:36 -0500

    Assignment completed

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -18,15 +18,39 @@ Use console.log() to clearly show the before-and-after type conversions.
 
 */
 
-
+//This works because implicit type conversion converts the string "5" into a number,
+//because it is a string which has a number subtracted from it.
 let result = "5" - 2;
 console.log("The result is: " + result);
 
+//This result does not work because the string "false" is truthy, and so when explicitly
+//converted to boolean, returns true.
 let isValid = Boolean("false");
 if (isValid) {
     console.log("This is valid!");
 }
 
+//This implictly converted 5 into a string as a result of being added to a string. 
+//Applying Number() to age fixes the problem.
 let age = "25";
-let totalAge = age + 5;
+let totalAge = Number(age) + 5;
 console.log("Total Age: " + totalAge);
+
+//Part 2
+
+//Implicit
+
+let weeksInYear = "52"
+let daysInWeek = 7;
+
+console.log(weeksInYear);
+console.log(daysInWeek);
+
+console.log(weeksInYear * daysInWeek);
+
+//Explict
+let placeHolder;
+
+console.log(placeHolder);
+
+console.log(String(placeHolder));
